76e4f633d5e8612a1c242b6241f20ce708ac2211
Jorge Dacosta <jorgedacostaza@gmail.com>
2019-10-26 11:42:05 +0100

    Returning JSON From getAlbum

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,6 +7,8 @@ export class ProductService {
   private _albumUrl = "../assets/album.json";
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {}
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl);
+  }
 
 }
