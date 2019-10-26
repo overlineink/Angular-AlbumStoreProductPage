bcf9a57b15f26126eead60a95bbffebb2a8573b6
Jorge Dacosta <jorgedacostaza@gmail.com>
2019-10-26 12:36:25 +0100

    Adding A Return Type To The getAlbum Method

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import "rxjs/add/operator/map";
+import { Observable } from "rxjs/Observable";
 import { Album } from "./album";
 
 @Injectable()
@@ -9,7 +10,7 @@ export class ProductService {
   private _albumUrl = "../assets/album.json";
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl)
 
     .map(response => response.json());
