6a0e6dc605c449b449dc874d4887d952e88eac58
Jorge Dacosta <jorgedacostaza@gmail.com>
2019-10-26 15:12:43 +0100

    Injecting A Private ProductService Object In The ProductListComponent

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,7 +10,7 @@ import { ProductService } from '../product.service';
 export class ProductListComponent implements OnInit {
 
 	products: Product[];
-  constructor() { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
   }
