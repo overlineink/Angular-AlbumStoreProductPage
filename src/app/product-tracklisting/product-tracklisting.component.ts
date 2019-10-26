8479e04afa32b25089e9f0f0a605a9ba8974de80
Jorge Dacosta <jorgedacostaza@gmail.com>
2019-10-26 14:01:47 +0100

    Injecting A Private ProductService Object In ProductTracklistingComponent

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -10,7 +10,7 @@ import { ProductService } from "../product.service";
 export class ProductTracklistingComponent implements OnInit {
 
   albumInfo: Album;
-  constructor() { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
   }
